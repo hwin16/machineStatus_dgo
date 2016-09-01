6c3d3338d415412a0eef5c3c2a2c3f0c2c7e4e0f
Htut Khine <htutkhwin@gmail.com>
2016-09-01 14:29:08 -0500

    Machine Status added.

--- machineStatus/models.py
+++ machineStatus/models.py
@@ -7,6 +7,7 @@ from django.utils.encoding import python_2_unicode_compatible
 class MachineStatus(models.Model): 
     machine_name = models.CharField(max_length=20)
     machine_state = models.CharField(max_length=50)
+    machine_status = models.CharField(max_length=50)
 
     def __str__(self): 
         return self.machine_name
